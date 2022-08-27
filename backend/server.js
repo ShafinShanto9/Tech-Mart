6e9d25661ce1c8cfea2aed53a527453378187978
shafin_shant0 <shafinshanto9@gmail.com>
2022-08-27 14:30:36 +0600

    initialize backend

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const {Server} = require('socket.io');
+const io = new Server(server, {
+  cors: '*',
+  methods: "*"
+})
+
+app.use(cors())
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+
+app.listen(5000, () => {
+    console.log("Port Running on port", 5000 );
+})
