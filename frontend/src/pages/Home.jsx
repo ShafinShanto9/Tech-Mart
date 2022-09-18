17eb72cadb8a44d7143ab5d77a8cf8a2fb9e0867
shafin_shant0 <shafinshanto9@gmail.com>
2022-09-18 12:01:11 +0600

    adding style"

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { LinkContainer } from "react-router-bootstrap";
+import categorise from '../resources/categorise.js'
+import { Row, Col } from 'react-bootstrap'
+import '../styles/Home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className=" d-flex justify-content-center align-items-center">
+      <div className='w-100'>
+        <img src='https://i.ibb.co/Jt83ChV/Shop-Now.png' className='home-banner w-100 h-50' />
+      {/* Featured Product */}
+      <div className='featured-product-container container mt-4'>
+        <h2>Latest Product</h2>
+      <div>
+        <Link to='/category/all'
+        style={{textDecoration:'none', textAlign:'right', display:'block'}}
+        >See More{'>>'}</Link>
+      </div>
+      </div>
+      {/* Sale Banner */}
+      <div className='sale__banner--container mt-4'>
+        <img src="https://i.ibb.co/y6htMHP/Untitled-design-1.png" className='w-100' alt="" />
+      </div>
+      <div className='recent-products-container container mt-4'>
+        <h2>Categorise</h2>
+        <Row>
+            {categorise?.map((category) => (
+                <LinkContainer to={`/category/${category.name.toLocaleLowerCase()}`}>
+                  <Col md={4}>
+                  <div style={{
+                    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), 
+                    url(${category?.img})`, gap: "10px"
+                  }} className="category-tile">
+                      {category?.name}
+                    </div>
+                  </Col>
+                </LinkContainer>
+              ))}
+        </Row>
+      </div>
+      </div>
+    </div>
   )
 }
 
---
+++ frontend/src/resources/categorise.js
@@ -0,0 +1,20 @@
+const categorise = [
+    {
+        name: 'Accessorise',
+        img:'https://i.ibb.co/yh2CbdW/71b-QEix-Gzh-L-AC-SL1500.jpg'
+    },
+    {
+        name: 'Phone',
+        img:'https://i.ibb.co/9GY1pgS/71-Fu-I8-Yv-CNL-AC-SL1500.jpg'
+    },
+    {
+        name: 'Laptop',
+        img: 'https://i.ibb.co/8XS5vbk/71x-XH7o-SNL-AC-SL1500.jpg',
+    },
+    {
+        name: 'Camera',
+        img: 'https://i.ibb.co/b6Nn9LR/61l-GUzxb5-JL-AC-SL1000.jpg',
+    },
+]
+
+export default categorise
