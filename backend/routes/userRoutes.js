36a659280515ec5174563cbd8cd3e076b9a16963
shafin_shant0 <shafinshanto9@gmail.com>
2022-09-19 14:58:46 +0600

    api working bug fixing

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -27,6 +27,42 @@ router.post('/login', async(req, res) => {
   }
 })
 
+router.get('/', async(req, res)=> {
+  try {
+    const users = await User.find({ isAdmin: false }).populate('orders');
+    res.json(users);
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
+})
+
+// get user orders
+
+// router.get('/:id/orders', async (req, res)=> {
+//   const {id} = req.params;
+//   try {
+//     const user = await User.findById(id).populate('orders');
+//     res.json(user.orders);
+//   } catch (e) {
+//     res.status(400).send(e.message);
+//   }
+// })
+// // update user notifcations
+// router.post('/:id/updateNotifications', async(req, res)=> {
+//   const {id} = req.params;
+//   try {
+//     const user = await User.findById(id);
+//     user.notifications.forEach((notif) => {
+//       notif.status = "read"
+//     });
+//     user.markModified('notifications');
+//     await user.save();
+//     res.status(200).send();
+//   } catch (e) {
+//     res.status(400).send(e.message)
+//   }
+// })
+
 
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const app = express();
 const http = require('http');
 require('./connection.js')
+
 const server = http.createServer(app);
 const {Server} = require('socket.io');
 const io = new Server(server, {
@@ -10,9 +11,15 @@ const io = new Server(server, {
   methods: "*"
 })
 
+
+const User = require('./models/User')
+const userRoutes = require('./routes/userRoutes')
+
 app.use(cors())
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+app.use('/users', userRoutes)
+
 
 
 app.listen(5000, () => {
